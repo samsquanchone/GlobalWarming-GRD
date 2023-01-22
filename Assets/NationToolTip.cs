3f5aa0947faddf27a2558b56b841098cb4ce2ba3
samsquanchone <105938519+samsquanchone@users.noreply.github.com>
2023-01-22 17:10:17 +0000

    Added GDP to nation tooltip

--- Assets/NationToolTip.cs
+++ Assets/NationToolTip.cs
@@ -6,12 +6,14 @@ using UnityEngine.EventSystems;
 
 //Was going to use polymorphism here an inherit the HoverTip, however, Ipointer enter and exit were not working for non UI, so decided to not inherit, may inherit this class for gameobject Tooltips 
 public class NationToolTip : MonoBehaviour
-{
+{   
+    //Tool tip variables
     protected string tipToShow;
     protected float timeToWait = 0.5f;
     private string dataToShow;
 
-
+    //Nation data
+    float gdp;
     int lumberMillsPlaced = 0;
     int activeTreeProjects = 0;
     int docksPlaced = 0;
@@ -47,17 +49,18 @@ public class NationToolTip : MonoBehaviour
 
     public void ShowMessage(string dataToShow)
     {
-        Debug.Log("Greetings");
+        
         UIHoverManager.OnMouseHover(dataToShow, Input.mousePosition);
     }
 
     public virtual void SetToolTipData()
     {
         string nationName;
-        float gdp;
+        
         
 
         nationName = gameObject.name;
+        //gdp = gameObject.GetComponent<Tile>();
 
         //Off the cuff way of doing this, probs not best to iterate over list every time mouse of UI - REFACTOR / CHANGE later
         foreach (GameObject obj in gameObject.GetComponent<Tile>().nationPlacedObjectsList)
@@ -80,13 +83,13 @@ public class NationToolTip : MonoBehaviour
                 trainStationsPlaced++;
             }
 
-            if (obj.name == "Oak(Clone)" || obj.name == "Bamboo(Clone)" || obj.name == "Willow(Clone)")
+            else if (obj.name == "Oak(Clone)" || obj.name == "Bamboo(Clone)" || obj.name == "Willow(Clone)")
             {
                 activeTreeProjects++;
             }
         }
 
-        dataToShow = nationName + "\n" + "Active Tree Projects: " + activeTreeProjects + "\n" + "Lumbermills: " + lumberMillsPlaced + "\n" + "Docks: " + docksPlaced + "\n" + "Train Stations: " + trainStationsPlaced;
+        dataToShow = nationName + "\n" + "GDP: £" + gdp.ToString() + "\n" + "Active Tree Projects: " + activeTreeProjects + "\n" + "Lumbermills: " + lumberMillsPlaced + "\n" + "Docks: " + docksPlaced + "\n" + "Train Stations: " + trainStationsPlaced;
 
         ShowMessage(dataToShow);
     }
