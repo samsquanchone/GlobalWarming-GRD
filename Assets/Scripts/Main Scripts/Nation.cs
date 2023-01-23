02e70522438631d7340983407a4c1e93302dea38
Enis Kaan Tasoren <eniskaan98@gmail.com>
2023-01-23 23:23:14 +0000

    Changes for All Base Systems

--- Assets/Scripts/Main Scripts/Nation.cs
+++ Assets/Scripts/Main Scripts/Nation.cs
@@ -4,21 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Nation : MonoBehaviour
+public class Nation : ScriptableObject
 {
     [SerializeField] public string Nation_Name;
 
+    [Header("Tiles")]
     //Nation's Tiles
     [SerializeField] public Tile[] Nations_Territories;
 
+    [Header("Colour")]
     //Nation Color
     [SerializeField] public Color32 Nation_Colour;
 
+    [Header("Nation Properties")]
     //GDP
     [SerializeField] public int GDP; //Each nation has its starting GDP tied to its cumulative population from the nation’s tiles. If nothing happens, nation GDP’s increases because of the steady increase of the population of its tiles.
 
     //Population
     [SerializeField] public int Cumilative_Population_From_Territories; //Nations does not affect populations, but populations effect the nation by increasing/decreasing its GDP and Awareness.
+    [SerializeField] public int Starting_Population;
 
     //Awereness
     [SerializeField] public float Awareness; //From 0.0 to 1.0
@@ -34,16 +38,37 @@ public class Nation : MonoBehaviour
      * (GDP represents the industrial capacity of the nation.) 
      * Nations pull wood from wood stockpile of the player and produce pykrete.*/
 
+    //Production Building Counts
+    [Header("Production Building Counts")]
+    [SerializeField] public int LEVEL_1_Lumbermill_Factory_Count;
+    [SerializeField] public int LEVEL_2_Lumbermill_Factory_Count;
+    [SerializeField] public int LEVEL_3_Lumbermill_Factory_Count;
+
+    [SerializeField] public int LEVEL_1_Tera_Factory_Count;
+    [SerializeField] public int LEVEL_2_Tera_Factory_Count;
+    [SerializeField] public int LEVEL_3_Tera_Factory_Count;
+
+    [Header("Country's Total Avaliable Woodland ")]
+    [SerializeField] public int Woodland_Count;
+
 
     private void Start()
     {
         //At the start of the game colour all territories according to the national colours.
         Colour_All_Teritories_According_to_the_Nation_Colour();
+
+        //Calculate Starting Pops
+        Calculate_Starting_Population();
     }
 
     public void Produce_Pykerete()
     {
         
+    }
+
+    public void Produce_Timber()
+    {
+
     }
 
 
@@ -64,7 +89,20 @@ public class Nation : MonoBehaviour
         }
     }
 
-    public void Change_Awareness_According_to_the_Nation()
+    public void Calculate_Starting_Population()
+    {
+        if(this.Nations_Territories != null)
+        {
+            for(int i =0; i< Nations_Territories.Length; i++)
+            {
+                this.Starting_Population += Nations_Territories[i].Population;
+            
+            }
+            this.Cumilative_Population_From_Territories = Starting_Population;
+        }
+    }
+
+    public void Change_Awareness_According_to_the_Nation_Pop_Heat_Levels_And_GDP()
     {
 
     }
--- Assets/Scripts/Main Scripts/Tile.cs
+++ Assets/Scripts/Main Scripts/Tile.cs
@@ -10,13 +10,9 @@ public class Tile : MonoBehaviour
 
     
 
-    [SerializeField] public int Habitability; //Between 0-100%
-        /*
-        Continental (Canada, Northern America, Europe, Northern China, and Japan): 95% Habitability	
-        Temperate or Tropical (Part of Europe, China, Most of Russia, Most of America, South Africa, Part of Mexico, Japan and Part of Canada): 100% Habitability
-        Dry (Northern Africa, Most of Mexica, Southern Africa): 80% Habitability
-        Polar or Desert (Northern Africa, Most of Mexica, Southern Africa): 50% Habitability
-        */
+    [SerializeField] public int Climate_Support; //Between 0-100%
+    /*Represents total max population allowance for the tile and life support. If the climate support gets reduced, pops on that tile starts to die.*/
+
 
     //Average Heat Level
     [SerializeField] public int Average_Heat_Level; //Global Warming, Events and Neutral Disasters effect heat levels. Heat levels directly effects Habitability level. 
@@ -28,9 +24,21 @@ public class Tile : MonoBehaviour
     [SerializeField] public Tree tree;
     [SerializeField] public int Tree_Age;
 
-    //Buildings (Is built or not)
+    //Is Avaliable
+    [SerializeField] public bool Tera_Factory_Avaliable;
     [SerializeField] public bool Harbor_Avaliable;
-    [SerializeField] public bool Autobahn;
-    [SerializeField] public bool Railway;
-    [SerializeField] public bool Airport;
+
+    //Buildigns and levels
+    //Production Buildings
+    [SerializeField] public int Lumbermill_Level = 0;
+    [SerializeField] public int Tera_Factory_Level = 0;
+
+    //Logistics Buildings
+    [SerializeField] public int Harbour_Level = 0;
+    [SerializeField] public int Railway_Level = 0;
+
+    private void Start()
+    {
+        this.Territory_Name = this.name;
+    }
 }
