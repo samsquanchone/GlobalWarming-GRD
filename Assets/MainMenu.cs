2beb0173b67f3174a13fb7235cd226906c825f42
Nikki789 <46848603+Nikki789@users.noreply.github.com>
2023-04-10 02:09:51 +0100

    Added How to play instructions in the main menu, fixes are needed (see desc)
    - To make buttons and images scalable inside panels
    - To optimize code in MainMenu.cs
    - To add a Game Interface Image
    - To fix bug of escape menu not closing properly
    - Possibly to add Postprocessing to scenes?

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,16 +5,76 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject moreInfoPanel;
+
+    //Scene 0 Main Menu Scene Functions
     public void PlayNewGame()
     {
         MenuData.SetGameType(true); //Start new game
-        SceneManager.LoadScene(1);
-        
+        SceneManager.LoadScene(5);
     }
 
     public void LoadGame()
     {
         MenuData.SetGameType(false); //Start new game
+        SceneManager.LoadScene(5);
+    }
+
+    public void HowToPlayGame()
+    {
+        MenuData.SetGameType(false); //Start new game
         SceneManager.LoadScene(1);
+    }
+
+    //All How to play game scenes Functions
+    public void onMoreInfoClick()
+    {
+        moreInfoPanel.gameObject.SetActive(!moreInfoPanel.gameObject.activeSelf);
+    }
+
+    //Scene 1 How To Play Scene 1 Functions
+    public void BackToMainMenu()
+    {
+        MenuData.SetGameType(false); //Start new game
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadHowToPlayGameScene2()
+    {
+        MenuData.SetGameType(false); //Start new game
+        SceneManager.LoadScene(2);
+    }
+
+    //Scene 2 How To Play Scene 2 Functions
+    public void BackToHowToPlayGameScene1()
+    {
+        MenuData.SetGameType(false); //Start new game
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadHowToPlayGameScene3()
+    {
+        MenuData.SetGameType(false); //Start new game
+        SceneManager.LoadScene(3);
+    }
+
+    //Scene 3 How To Play Scene 3 Functions
+    public void BackToHowToPlayGameScene2()
+    {
+        MenuData.SetGameType(false); //Start new game
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadHowToPlayGameScene4()
+    {
+        MenuData.SetGameType(false); //Start new game
+        SceneManager.LoadScene(4);
+    }
+
+    //Scene 4 How To Play Scene 4 Functions
+    public void BackToHowToPlayGameScene3()
+    {
+        MenuData.SetGameType(false); //Start new game
+        SceneManager.LoadScene(3);
     }
 }
