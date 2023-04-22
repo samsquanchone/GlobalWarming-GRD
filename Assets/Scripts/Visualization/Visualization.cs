b922f2088c9db22ff2c8bd9f29481909108ac0fd
Enis Kaan Tasoren <eniskaan98@gmail.com>
2023-04-22 14:55:53 +0100

    Adjustments to URP and Visualization

--- Assets/Scripts/Visualization/Visualization.cs
+++ Assets/Scripts/Visualization/Visualization.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Visualization : MonoBehaviour
 {
+
+    [SerializeField] GameObject Money_Model;
+    [SerializeField] GameObject Population_Model;
+
+    Nation[] All_Nations; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        All_Nations = GameObject.FindObjectsOfType<Nation>();
+        Debug.Log("Nation Count: " + All_Nations.Length);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Visualize_Population()
     {
-        
+        for(int i = 0; i < All_Nations.Length; i++)
+        {
+            Instantiate(Population_Model, All_Nations[i].Nations_Territories[0].transform.position, Quaternion.identity);
+        }
+    }
+
+    public void Visualize_GDP()
+    {
+        for (int i = 0; i < All_Nations.Length; i++)
+        {
+            Instantiate(Money_Model, All_Nations[i].Nations_Territories[0].transform.position, Quaternion.identity);
+        }
     }
 }
