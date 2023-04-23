49558c5bffd5898f328d39acfa4c0fe49ce1e49b
Enis Kaan Tasoren <eniskaan98@gmail.com>
2023-04-23 22:09:25 +0100

    Waypoint System

--- Assets/Scripts/Visualization/Waypoint System/NodeManager.cs
+++ Assets/Scripts/Visualization/Waypoint System/NodeManager.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class NodeManager : MonoBehaviour
 {
-    Node[] Sea_Nodes;
-    Node[] Land_Nodes;
+    [SerializeField] Node[] Sea_Nodes;
+    [SerializeField] Node[] Land_Nodes;
 
-    GameObject Ship;
-    GameObject Train;
-    void Start()
+    [SerializeField] GameObject Ship;
+    [SerializeField] GameObject Train;
+
+/*    [SerializeField] int Number_of_Ships = 0;
+    [SerializeField] int Number_of_Trains = 0;*/
+
+
+    public void Add_Ship()
     {
-        
+        int NewSpawnNodeNumber = Random.Range(0, Sea_Nodes.Length - 1);
+        GameObject SpawnedShipObject = Instantiate(Ship, Sea_Nodes[NewSpawnNodeNumber].transform.position , Quaternion.identity);
+        SpawnedShipObject.GetComponent<Vehicle>().TargetNode = Sea_Nodes[NewSpawnNodeNumber];
+        SpawnedShipObject.transform.LookAt(Sea_Nodes[NewSpawnNodeNumber].transform.position);
     }
 
-    
-    void Update()
+    public void Add_Train()
     {
-        
+        int NewSpawnNodeNumber = Random.Range(0, Land_Nodes.Length - 1);
+        GameObject SpawnedShipObject = Instantiate(Train, Land_Nodes[NewSpawnNodeNumber].transform.position, Quaternion.identity);
+        SpawnedShipObject.GetComponent<Vehicle>().TargetNode = Land_Nodes[NewSpawnNodeNumber];
+        SpawnedShipObject.transform.LookAt(Land_Nodes[NewSpawnNodeNumber].transform.position);
     }
 }
